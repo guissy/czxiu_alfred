596b7fe9629fa726573036a57215c8238a99a12e
桂子 <guissy@qq.com>
2019-04-13 14:30:13 +0800

    fix char break line

--- build.py
+++ build.py
@@ -13,16 +13,16 @@ import datetime
 import os
 import subprocess
 
-s1, _id = json.loads(sys.argv[1])
+s1, type = json.loads(sys.argv[1])
 
-query = dict(id=_id)
 csrf = 'iU69WvH7fJqOYcPH03_UAWHPGBHOtaJLEVUheuW99lS5CswUqLEa6vZRq-qbD7tjCPlNQOON1nhBABgshIiSGA%3D%3D'
-if id == 'nv_lei':
+if type == 'nv_lei':
     # 女泪
     # https://www.czxiu.com/cz/diy-1052.html#main
+    _id = 1560
     params = {
         '_csrf': csrf,
-        'ZDiyForm[xpos]': 10,
+        'ZDiyForm[xpos]': int(340 / 2 - min((len(s1), 14)) * 12.7),
         'ZDiyForm[ypos]': 178,
         'ZDiyForm[image]': '/assets/7bbc7da5/material/diy/old/1052.gif',
         'ZDiyForm[text]': s1,
@@ -35,12 +35,15 @@ if id == 'nv_lei':
         'ZDiyForm[move_type]': 'none',
         'ZDiyForm[hideSignature]': 0
     }
-elif id == 'zhong_ju_quan':
+elif type == 'zhong_ju_quan':
     # 众举拳
     # https://www.czxiu.com/cz/diy-690.html
+    _id = 1085
+    n = 0 if len(s1) % 2 == 0 else 12
+    s1 = s1.center(10, ' ')
     params = {
         '_csrf': csrf,
-        'ZDiyForm[xpos]': 10,
+        'ZDiyForm[xpos]': 8 + n,
         'ZDiyForm[ypos]': 11,
         'ZDiyForm[image]': '/assets/7bbc7da5/material/diy/old/690.gif',
         'ZDiyForm[text]': s1,
@@ -53,6 +56,29 @@ elif id == 'zhong_ju_quan':
         'ZDiyForm[move_type]': 'none',
         'ZDiyForm[hideSignature]': 0
     }
+elif type == 'ke_ai_bao_bao':
+    # 可爱宝宝
+    # https://www.czxiu.com/cz/diy-1063.html#main
+    _id = 1571
+    s1 = '\r\n'.join(s1)
+    params = {
+        '_csrf': csrf,
+        'ZDiyForm[xpos]': 192,
+        'ZDiyForm[ypos]': 14,
+        'ZDiyForm[image]': '/assets/7bbc7da5/material/diy/old/1063.gif',
+        'ZDiyForm[text]': s1,
+        'ZDiyForm[font]': 'simhei.ttf',
+        'ZDiyForm[color]': '#000000',
+        'ZDiyForm[color2]': '',
+        'ZDiyForm[size]': 16,
+        'ZDiyForm[size_zoom]': 0,
+        'ZDiyForm[repeat_type]': 'shadow',
+        'ZDiyForm[repeat_size]': 1,
+        'ZDiyForm[repeat_color]': '#ffffff',
+        'ZDiyForm[repeat_color2]': '',
+        'ZDiyForm[move_type]': 'none',
+        'ZDiyForm[hideSignature]': 0
+    }
 elif type == 'warn_board':
     # 警告牌
     # https://www.czxiu.com/cz/wworning.html#main
@@ -62,25 +88,6 @@ elif type == 'warn_board':
         'ZMultiForm[name]': '警告',
         'ZMultiForm[name2]': s1,
         'ZMultiForm[hideSignature]': 0
-    }
-elif type == 'forest_backboard':
-    # 森林黑板
-    # https://www.czxiu.com/cz/diy-696.html#main
-    _id = 1091
-    params = {
-        '_csrf': csrf,
-        'ZDiyForm[xpos]': 82,
-        'ZDiyForm[ypos]': 30,
-        'ZDiyForm[image]': '/assets/7bbc7da5/material/diy/old/696.gif',
-        'ZDiyForm[text]': s1,
-        'ZDiyForm[font]': 'fzyt.ttf',
-        'ZDiyForm[color]': '#ffffff',
-        'ZDiyForm[color2]': '',
-        'ZDiyForm[size]': 26,
-        'ZDiyForm[size_zoom]': 0,
-        'ZDiyForm[repeat_type]': 'none',
-        'ZDiyForm[move_type]': 'none',
-        'ZDiyForm[hideSignature]': 0,
     }
 elif type == 'mao_mi_ju_zhua':
     # 猫咪举爪
@@ -104,30 +111,31 @@ elif type == 'mao_mi_ju_zhua':
         'ZDiyForm[move_type]': 'none',
         'ZDiyForm[hideSignature]': 0,
     }
-elif type == 'ke_ai_bao_bao':
-    # 可爱宝宝
-    # https://www.czxiu.com/cz/diy-1063.html#main
-    _id = 1571
+elif type == 'forest_backboard':
+    # 森林黑板
+    # https://www.czxiu.com/cz/diy-696.html#main
+    _id = 1091
+    s2 = ''
+    for i in range(0, len(s1), 5):
+        s2 += s1[i:i + 5] + '\r\n'
     params = {
         '_csrf': csrf,
-        'ZDiyForm[xpos]': 192,
-        'ZDiyForm[ypos]': 14,
-        'ZDiyForm[image]': '/assets/7bbc7da5/material/diy/old/1063.gif',
-        'ZDiyForm[text]': s1,
-        'ZDiyForm[font]': 'simhei.ttf',
-        'ZDiyForm[color]': '#000000',
+        'ZDiyForm[xpos]': 82,
+        'ZDiyForm[ypos]': 30,
+        'ZDiyForm[image]': '/assets/7bbc7da5/material/diy/old/696.gif',
+        'ZDiyForm[text]': s2,
+        'ZDiyForm[font]': 'fzyt.ttf',
+        'ZDiyForm[color]': '#ffffff',
         'ZDiyForm[color2]': '',
-        'ZDiyForm[size]': 16,
+        'ZDiyForm[size]': 26,
         'ZDiyForm[size_zoom]': 0,
-        'ZDiyForm[repeat_type]': 'shadow',
-        'ZDiyForm[repeat_size]': 1,
-        'ZDiyForm[repeat_color]': '#ffffff',
-        'ZDiyForm[repeat_color2]': '',
+        'ZDiyForm[repeat_type]': 'none',
         'ZDiyForm[move_type]': 'none',
-        'ZDiyForm[hideSignature]': 0
+        'ZDiyForm[hideSignature]': 0,
     }
 else:
-    id = 'ju_pai_xiao_ren'
+    type = 'ju_pai_xiao_ren'
+    _id = 1700
     # 举牌小人
     # https://www.czxiu.com/cz/2017jupai.html
     s1 = s1.replace('，', '\r\n')
@@ -136,6 +144,7 @@ else:
     s1 = s1.replace('；', '\r\n')
     s1 = s1.replace('？', '\r\n')
     s1 = s1.replace('！', '\r\n')
+    s1 = s1.replace(' ', '\r\n')
     params = {
         '_csrf': csrf,
         'ZMultiForm[text]': s1,
@@ -143,6 +152,7 @@ else:
         'ZMultiForm[hideSignature]': 0
     }
 
+query = dict(id=_id)
 payload = urllib.urlencode(params)
 headers = {
     'accept': "application/json, text/javascript, */*; q=0.01",
@@ -160,16 +170,17 @@ headers = {
                   "Chrome/74.0.3729.6 Safari/537.36",
     'content-type': "application/x-www-form-urlencoded"
 }
+# raise Exception('', _id)
 response = web.post('https://www.czxiu.com/style/make', data=payload, headers=headers, params=query)
-img = 'https://www.czxiu.com/' + response.json()['url']
-file_name = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S.gif')
-file_path = os.path.join('/tmp', file_name)
-web.get(img).save_to_path(file_path)
-wf = Workflow()
-wf.add_item(title='ok',
-            arg=file_path,
-            valid=True,
-            icon=ICON_WEB)
-wf.send_feedback()
-
-subprocess.call(['./clipfile', file_name])
+if response.status_code != 200:
+    sys.stdout.write('网络不畅或者服务不可用')
+data = response.json()
+if 'url' in data:
+    img = 'https://www.czxiu.com/' + data['url']
+    file_name = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S.gif')
+    file_path = os.path.join('/tmp', file_name)
+    web.get(img).save_to_path(file_path)
+    subprocess.call(['./clipfile', file_name])
+else:
+    sys.stdout.write(response.text)
+sys.stdout.write('生成的表情到剪切板了')
